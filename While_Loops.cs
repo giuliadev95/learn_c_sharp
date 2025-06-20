ae8ecbe8c5eb57406892d27a3efa383cc80c86b9
giulia_moukouyou <giuliamoukouyou@gmail.com>
2025-06-20 09:38:33 +0200

    Add exercise with While loops

---
+++ While_Loops.cs
@@ -0,0 +1,21 @@
+﻿// While Loop
+
+using System;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while ( index <= 5 )
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
